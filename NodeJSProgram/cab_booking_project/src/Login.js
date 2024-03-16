c80944e179862095b1d2ca1468e0bf3c4f7374fb
Dev Patel <devpatel1828king@gmail.com>
2024-03-16 15:58:19 +0530

    Designed Login Page

--- NodeJSProgram/cab_booking_project/src/Login.js
+++ NodeJSProgram/cab_booking_project/src/Login.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import "./Login.css";
+
+
+
 
 const Login = () => {
     return (
-        <div className="content">
-            <h2>Login</h2>
-            <p>Choose how you want to login:</p>
-            <div className='"navbar-item"'>
-                <Link to="/login/driver" className='nav-item link'>Login as Driver</Link>
+        
+        <>
+            {/* <body style={{ backgroundColor: 'black', height: '100vh' }}></body>     */}
+
+            <div className="content">
+                <div className="text-content" data-aos ="slide-right">
+                   
+                    
+                   
+
+                    <br />
+                    <br />
+                    <button className="new_button">Log in as Driver</button>
+
+
+                </div>
+                <div className="new_image-content" data-aos ="fade-up">
+                <img src={require('./login_driver.png')} alt="Image 1" height={400} width={500}/>
+                </div>
             </div>
-            <div className='"navbar-item"'>
-                <Link to="/login/passenger" className='nav-item link'>Login as Passenger</Link>
+            <div className="new_reverse-content">
+                <div className="new_reverse-text-content" data-aos ="slide-left">
+                <button className="new_button">Log in as Passenger</button>
+                    <br />
+                    <br />
+                </div>
+                <div className="new_reverse-image-content" data-aos ="fade-up">
+                <img src={require('./passenger_login.png')} alt="Image 1" height={400} width={500}/>
+                </div>
             </div>
-        </div>
+
+            
+
+
+        
+        </>        
     );
 }
 
