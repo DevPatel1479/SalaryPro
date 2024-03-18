51b6e69a471d8450abc822596a55c6a1a2cccef6
Dev Patel <devpatel1828king@gmail.com>
2024-03-18 22:07:43 +0530

    Change the order of SignUp buttons

--- CabBooking/cab_booking_project/src/Signup.js
+++ CabBooking/cab_booking_project/src/Signup.js
@@ -9,24 +9,24 @@ const Signup = () => {
                 <div className="text-content" data-aos="slide-right">
                     <br />
                     <br />
-                    <Link to="/signup/driver">
-                        <button className="new_button">Sign Up as Driver</button>
+                    <Link to="/signup/passenger">
+                        <button className="new_button">Sign Up as Passenger</button>
                     </Link>
                 </div>
                 <div className="new_image-content" data-aos="fade-up">
-                    <img src={require('./Driver_SignUp.png')} alt="Driver SignUp" height={400} width={500} />
+                    <img src={require('./Passenger_SignUp.png')} alt="Passenger SignUp" height={400} width={500} />
                 </div>
             </div>
             <div className="new_reverse-content">
                 <div className="new_reverse-text-content" data-aos="slide-left">
-                    <Link to="/signup/passenger">
-                        <button className="new_button">Sign Up as Passenger</button>
+                    <Link to="/signup/driver">
+                        <button className="new_button">Sign Up as Driver</button>
                     </Link>
                     <br />
                     <br />
                 </div>
                 <div className="new_reverse-image-content" data-aos="fade-up">
-                    <img src={require('./Passenger_SignUp.png')} alt="Passenger SignUp" height={400} width={500} />
+                    <img src={require('./Driver_SignUp.png')} alt="Driver SignUp" height={400} width={500} />
                 </div>
             </div>
             {/* <Routes>
